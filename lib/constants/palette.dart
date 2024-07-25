4f7e3ce61c00663c8d87cd1680587edd86e69b1e
vdr <valeria.do@gmail.com>
2024-07-25 08:43:50 -0500

    created color palette

---
+++ lib/constants/palette.dart
@@ -0,0 +1,21 @@
+/// Colors taken from this color palette:
+/// https://lospec.com/palette-list/crayola84
+
+import 'package:flutter/material.dart';
+
+class Palette {
+  Color get kBlue => const Color(0xff1d75fb);
+  Color get kDarkBlue => const Color(0xFF0050bc);
+  Color get kRedAccent => const Color(0xFFd10841);
+  Color get kInkDarkOpascity => const Color(0xff352b42);
+  Color get kInk => const Color(0xee352b42);
+  Color get kDarkestGray => const Color(0xff2d383a); // application background
+  Color get kDeepPurpleAccent => const Color(0xff672940); // application accents
+  Color get kPurplePink => const Color(0xffd811b9); 
+  Color get kYellow => const Color(0xffffffd1);
+  Color get kGreen => const Color(0xffa2dcc7);
+  Color get kYellow2 => const Color(0xffffebb5);
+  Color get kPurpleSoft => const Color(0xffffd7ff);
+  Color get kYellowBlue => const Color(0xffbfc8e3);
+  Color get trueWhite => const Color(0xffffffff);
+}
