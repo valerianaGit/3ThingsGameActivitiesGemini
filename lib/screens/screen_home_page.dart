0d853d88f6f7a9431a680b3706dd84a6decc190d
vdr <valeria.do@gmail.com>
2024-07-25 08:15:37 -0500

    stage project

---
+++ lib/screens/screen_home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScreenHomePage extends StatelessWidget {
+  const ScreenHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
