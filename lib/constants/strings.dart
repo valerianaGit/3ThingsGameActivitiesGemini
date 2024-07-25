babff1a871d81ad34d1ea8e3d9586b6f5c606b51
vdr <valeria.do@gmail.com>
2024-07-25 09:32:46 -0500

    create screen strings

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,1 +1,11 @@
 const kGameTitle = 'Three Things';
+
+const kBreath = 'Breath';
+
+const kFear = 'Fear';
+
+const kGratitude = 'Gratitude';
+
+const kTodo = 'Todo';
+
+const kGrounding = 'Grounding';
